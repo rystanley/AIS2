31e753f9f520e36da3df7826c6936ee2b9e588b0
Homarus23 <rstanley@mun.ca>
2017-06-09 22:00:01 -0300

    updates

--- code/ProcessCode.R
+++ code/ProcessCode.R
@@ -14,7 +14,6 @@ temp[,3] <- gsub(".txt","",temp[,3]) #get rid of '.txt' at the end of the file c
 #this is a vector which identifies the files that are unique to eachother
 uniqueData <- unique(temp[,3])
 
-
 ## Read in ship metadata files
 mdata_files <- paste0("data/",files[grep("Msg5",files)])
 
@@ -24,9 +23,21 @@ mdata <- subset(mdata,!duplicated(mdata$MMSI)) # get rid of any duplicateed ship
 mdata <- mdata[,c("MMSI","IMONumber","ShipType","ShipLength","ShipBeam","Draft")] #trim data not required
 
 
+#positoinal datafiles
 datafiles <- paste0("data/",files[grep("Msg1",files)])
 
+#create folder to dump files
+if(length(which(list.files(getwd())=="MMSI_files"))==0){dir.create(paste0(getwd(),"/MMSI_files"))} 
+
+#Warning message to make sure you don't make super large files by accident. 
+if(!length(which(list.files(getwd())=="MMSI_files"))==0 & length(dir("MMSI_files/"))>0){
+      print(paste0("Before running the next loop make sure all files contained in ",
+                   getwd(),"/MMSI_files are supposed to be there, else they will have extra data added to them."))} 
+
+
 ##Loop through and create files.
+
+Start <- Sys.time() # Start timer
 
 for(ind in datafiles){
   
@@ -41,8 +52,6 @@ for(ind in datafiles){
   
   MMSIunique <- unique(temp$MMSI)
   
-  #create folder to dump files
-  if(length(which(list.files(getwd())=="MMSI files"))==0){dir.create(paste0(getwd(),"/MMSI_files"))} 
   
   for (i in MMSIunique){
     dplyr::filter(temp,MMSI==i)%>%fwrite(.,file = paste0("MMSI_files/",i,".txt"),append=T)
@@ -50,13 +59,19 @@ for(ind in datafiles){
   
 }
 
+Start-Sys.time() #end timer
+
+
+shipfiles <- dir("MMSI_files/")
+shipfiles <- as.numeric(gsub(".txt","",shipfiles))
+
 ## Merge each file with MMSI
-# for (ind in datafiles){
-#   
-#   temp <- temp <- fread(ind,header=T, stringsAsFactors=FALSE)
-#   temp <- merge(temp,mdata,by="MMSI")
-#   
-#   #S
+for (ind in datafiles){
+   
+   temp <- temp <- fread(paste0("MMSI_files/",ind),header=T, stringsAsFactors=FALSE)
+   temp <- merge(temp,mdata,by="MMSI")
+   
+   
 #   
 #   
 #   
